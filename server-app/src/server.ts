33bc9722508e6a2101c7d80a2270f61c648abd94
Shreyansh <shreyansh2128@gmail.com>
2025-02-20 17:19:45 +0530

    route updates

--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -13,11 +13,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors( 
-   {origin: 'http://localhost:3000'
-  , credentials: true} ));
+   {origin: '*'} ));
 
 app.use("/api/",shortUrl)
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
